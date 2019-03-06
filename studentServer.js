83a2ff4e38349375f21f8c52f83f06d8938b07a2
livirhind <livi.rhind@gmail.com>
2019-03-06 04:50:57 -0500

    another attempt at fixing connection

--- studentServer.js
+++ studentServer.js
@@ -42,23 +42,22 @@ app.use(function(req,res,next){
 
 
 //testing the connection
-app.get('/postgistest', function(req,res){
-	pool.connect(function(err,client,done){
-		if(err){
-			console.log("not able to get connection"+ err);
-			res.status(400).send(err);
-		}
-		client.query('SELECT name FROM london_poi', function(err,result){
-			done();
-			if(err){
-				console.log(err);
-				res.status(400).send(err);
-			}
-			res.status(200).send(result.rows);
-		});
-	});
+app.get('/postgistest', function (req,res) {
+pool.connect(function(err,client,done) {
+if(err){
+console.log("not able to get connection "+ err);
+res.status(400).send(err);
+}
+client.query('SELECT name FROM london_poi' ,function(err,result) {
+done();
+if(err){
+console.log(err);
+res.status(400).send(err);
+}
+res.status(200).send(result.rows);
 });
-
+});
+});
 
 app.post('/reflectData', function(req,res){
 	//note that we are using POST here as we are uploading data 
