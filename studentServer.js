d737aea5d5e1c1a2ffdb5de0676d28589f0554b0
livirhind <livi.rhind@gmail.com>
2019-03-06 09:37:09 -0500

    rearranging order of functions

--- studentServer.js
+++ studentServer.js
@@ -1,27 +1,13 @@
 //express is the serve that forms part of the nodejs program
 var express = require('express');
-var path =require("path");
 var app=express();
 //add an http server to serve files to the Edge browser
 //due to certificate issues it rejects the https files if they are not directly called in a typed URL
 var http = require('http');
-var httpServer = http.createServer(app);
-httpServer.listen(4480);
-app.get('/',function(req,res){
-	res.send("hello world from the HTTP server");
-});
-
-//functionality to process the form data
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
-app.use(bodyParser.json());
-
-//setting up database connectivity
 var fs= require('fs');
-var pg = require('pg');
+var httpServer = http.createServer(app);
 var configtext = "" + fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
+
 //now convert the configuration file into the correct format -i.e. a name/value pair array
 var configarray = configtext.split(",");
 var config = {};
@@ -30,8 +16,16 @@ for (var i = 0; i < configarray.length; i++){
 	config[split[0].trim()]=split[1].trim();
 }
 console.log(config);
+var pg = require('pg');
+
 var pool = new pg.Pool(config);
- 
+//functionality to process the form data
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
+
 
 //functionality to allow cross-domain queries when PhoneGap is running a server
 app.use(function(req,res,next){
@@ -41,6 +35,15 @@ app.use(function(req,res,next){
     next();
 });
 
+
+app.post('/reflectData',function(req,res){
+  // note that we are using POST here as we are uploading data
+  // so the parameters form part of the BODY of the request rather than the RESTful API
+  console.dir(req.body);
+
+  // for now, just echo the request back to the client
+  res.send(req.body);
+});
 
 //testing the connection
 app.get('/postgistest', function (req,res) {
@@ -87,6 +90,9 @@ app.post('/uploadData', function(req,res){
 	});
 
 
+});
+app.get('/',function(req,res){
+	res.send("hello world from the HTTP server");
 });
 
 
