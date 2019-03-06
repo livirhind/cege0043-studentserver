12e450f55c310611faeb75219c9326615ec64e7b
livirhind <livi.rhind@gmail.com>
2019-03-06 11:16:53 -0500

    debuggin student server

--- studentServer.js
+++ studentServer.js
@@ -1,12 +1,14 @@
 //express is the serve that forms part of the nodejs program
 var express = require('express');
-var app=express();
+var path = require("path");
+var app = express();
 //add an http server to serve files to the Edge browser
 //due to certificate issues it rejects the https files if they are not directly called in a typed URL
-var http = require('http');
-var fs= require('fs');
-var httpServer = http.createServer(app);
+var fs = require('fs');
+var pg = require('pg');
+
 var configtext = "" + fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
+
 
 //now convert the configuration file into the correct format -i.e. a name/value pair array
 var configarray = configtext.split(",");
@@ -15,8 +17,6 @@ for (var i = 0; i < configarray.length; i++){
 	var split = configarray[i].split(':');
 	config[split[0].trim()]=split[1].trim();
 }
-console.log(config);
-var pg = require('pg');
 
 var pool = new pg.Pool(config);
 //functionality to process the form data
@@ -26,7 +26,24 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-httpServer.listen(4480);
+
+
+app.get('/postgistest', function (req,res) {
+pool.connect(function(err,client,done) {
+if(err){
+console.log("not able to get connection "+ err);
+res.status(400).send(err);
+}
+client.query('SELECT name FROM london_poi' ,function(err,result) {
+done();
+if(err){
+console.log(err);
+res.status(400).send(err);
+}
+res.status(200).send(result.rows);
+});
+});
+});
 
 //functionality to allow cross-domain queries when PhoneGap is running a server
 app.use(function(req,res,next){
@@ -37,55 +54,68 @@ app.use(function(req,res,next){
 });
 
 
-app.post('/reflectData',function(req,res){
-  // note that we are using POST here as we are uploading data
-  // so the parameters form part of the BODY of the request rather than the RESTful API
-  console.dir(req.body);
-
-  // for now, just echo the request back to the client
-  res.send(req.body);
+app.post('/reflectData', function (req, res) {
+    // note that we are using POST here as we are uploading data
+    // so the parameters form part of the BODY of the request rather
+    //than the RESTful API
+    console.dir(req.body);
+    // for now, just echo the request back to the client
+    res.send(req.body);
 });
 
 
+app.post('/uploadData', function (req, res) {
+    // note that we are using POST here as we are uploading data
+    // so the parameters form part of the BODY of the request rather than the RESTful API
+    console.dir(req.body);
+    pool.connect(function (err, client, done) {
+        if (err) {
+            console.log("not able to get connection " + err);
+            res.status(400).send(err);
+        }
+        var name = req.body.name;
+        var surname = req.body.surname;
+        var modulecode = req.body.modulecode;
+        var portnum = req.body.port_id;
+        var language = req.body.language;
+        var modulelist = req.body.modulelist;
+        var lecturetime = req.body.lecturetime;
+        var geometrystring = "st_geomfromtext('POINT(" + req.body.longitude + " " + req.body.latitude + ")')";
 
-app.post('/uploadData', function(req,res){
-	//note that we are using POST here as we are uploading data 
-	//so the parameters form part of the BODY of the request rather than the RESTful API
-	console.dir(req.body);
+        var querystring = "INSERT into formdata (name,surname,module, port_id,language, modulelist, lecturetime, geom) values ($1,$2,$3,$4,$5,$6,$7,";
+        var querystring = querystring + geometrystring + ") ";
+        console.log(querystring);
+        client.query(querystring, [name, surname, modulecode, portnum, language, modulelist, lecturetime], function (err, result) {
+            done();
+            if (err) {
+                console.log(err);
+                res.status(400).send(err);
+            }
+            res.status(200).send("row inserted");
 
-	pool.connect(function(err,client,done){
-		if(err){
-			console.log("not able to get connection"+err);
-			res.status(400).send(err);
-		}
-		var name = req.body.name;
-		var surname = req.body.surname;
-		var modulecode=req.body.modulecode;
-		var portnum = req.body.port_id;
-		var language = req.body.language;
-		var modulelist=req.body.modulelist;
-		var lecturetime = req.body.lecturetime;
-		var geometrystring = "st_geomfromtext ('POINT("+req.body.longitude+" "+req.body.latitude+")')";
-		var querystring = "INSERT into formdata (name,surname,modulecode,port_id,language,modulelist,lecturetime,geom) values ($1,$2,$3,$4,$5,$6,$7,";
-		var querystring = querystring + geometrystring + ")";
-		console.log(querystring);
-		client.query(querystring,[name, surname, modulecode,portnum,language,modulelist,lecturetime], function(err,result){
-			done();
-			if(err){
-				console.log(err);
-				res.status(400).send(err);
-			}
-             res.status.send("row inserted");
-		});
-	});
-
+        });
+    });
 
 });
+
 app.get('/',function(req,res){
 	res.send("hello world from the HTTP server");
 });
 
+//modify the code for a cross origin request
+app.use(function(req,res,next){
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
+});
 
+//log the requests for debugging
+app.use(function(req,res,next){
+	var filename = path.basename(req.url);
+	var extension = path.extname(filename);
+	console.log("The file "+ filename + " was requested.");
+	next();
+});
 //serve static files e.g. html, css 
 //this should always be the last line in the server file
 app.use(express.static(__dirname));
