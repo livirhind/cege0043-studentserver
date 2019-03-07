1578a2f47d349f3158fe4c685b95c1df15efa2de
livirhind <livi.rhind@gmail.com>
2019-03-07 06:29:59 -0500

    adding http listen

--- studentServer.js
+++ studentServer.js
@@ -1,7 +1,20 @@
 //express is the serve that forms part of the nodejs program
 var express = require('express');
+
+
 var path = require("path");
 var app = express();
+
+//modify the code for a cross origin request
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+var http = require('http');
+var httpServer = http.createServer(app);
+httpServer.listen(4480); 
 //add an http server to serve files to the Edge browser
 //due to certificate issues it rejects the https files if they are not directly called in a typed URL
 var fs = require('fs');
@@ -30,19 +43,19 @@ app.use(bodyParser.json());
 
 app.get('/postgistest', function (req,res) {
 pool.connect(function(err,client,done) {
-if(err){
-console.log("not able to get connection "+ err);
+       if(err){
+        console.log("not able to get connection "+ err);
 res.status(400).send(err);
 }
 client.query('SELECT name FROM london_poi' ,function(err,result) {
-done();
-if(err){
-console.log(err);
-res.status(400).send(err);
+      done();
+        if(err){
+                console.log(err);
+             res.status(400).send(err);
 }
 res.status(200).send(result.rows);
-});
-});
+       });
+   });
 });
 
 //functionality to allow cross-domain queries when PhoneGap is running a server
@@ -102,12 +115,6 @@ app.get('/',function(req,res){
 	res.send("hello world from the HTTP server");
 });
 
-//modify the code for a cross origin request
-app.use(function(req,res,next){
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
-	next();
-});
 
 //log the requests for debugging
 app.use(function(req,res,next){
