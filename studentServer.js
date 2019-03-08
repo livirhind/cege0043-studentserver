67b8733c3ace4d67774bc53a64d452a49002b5a7
livirhind <livi.rhind@gmail.com>
2019-03-08 09:24:08 -0500

    debugging geojson

--- studentServer.js
+++ studentServer.js
@@ -1,10 +1,10 @@
 //express is the serve that forms part of the nodejs program
 var express = require('express');
-
-
 var path = require("path");
 var app = express();
-
+var http = require('http');
+var httpServer = http.createServer(app);
+httpServer.listen(4480); 
 //modify the code for a cross origin request
 app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin", "*");
@@ -12,9 +12,11 @@ app.use(function(req,res,next){
     next();
 });
 
-var http = require('http');
-var httpServer = http.createServer(app);
-httpServer.listen(4480); 
+app.get('/',function(req,res){
+    res.send("hello world from the HTTP server");
+});
+
+
 //add an http server to serve files to the Edge browser
 //due to certificate issues it rejects the https files if they are not directly called in a typed URL
 var fs = require('fs');
@@ -58,14 +60,6 @@ res.status(200).send(result.rows);
    });
 });
 
-//functionality to allow cross-domain queries when PhoneGap is running a server
-app.use(function(req,res,next){
-	res.header("Access-Control-Allow-Origin","*");
-	res.header("Access-Control-Allow-Headers","X-Requested-With");
-    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    next();
-});
-
 
 app.post('/reflectData', function (req, res) {
     // note that we are using POST here as we are uploading data
@@ -94,6 +88,14 @@ app.post('/uploadData', function (req, res) {
         var modulelist = req.body.modulelist;
         var lecturetime = req.body.lecturetime;
         var geometrystring = "st_geomfromtext('POINT(" + req.body.longitude + " " + req.body.latitude + ")')";
+        
+        console.log(name)
+        console.log(surname)
+        console.log(module)
+        console.log(portnum)
+        console.log(language)
+        console.log(modulelist)
+        console.log(geometrystring)
 
         var querystring = "INSERT into formdata (name,surname,module, port_id,language, modulelist, lecturetime, geom) values ($1,$2,$3,$4,$5,$6,$7,";
         var querystring = querystring + geometrystring + ") ";
@@ -104,30 +106,60 @@ app.post('/uploadData', function (req, res) {
                 console.log(err);
                 res.status(400).send(err);
             }
-            res.status(200).send("row inserted");
-
-        });
+            res.status(200).send("row inserted"); });
     });
 
 });
 
-app.get('/getFormData/:port_id', function (req,res) {
-      pool.connect(function(err,client,done) {
-    if(err){
-   console.log("not able to get connection "+ err);
+
+app.get('/getGeoJSON/:tablename/:geomcolumn/:portNumber?', function (req,res) {
+pool.connect(function(err,client,done) {
+if(err){
+console.log("not able to get connection "+ err);
 res.status(400).send(err);
-  }
-// use the inbuilt geoJSON functionality
-// and create the required geoJSON format using a query adapted from here:
-//Collections-with-JSON-and-PostGIS-functions.html, accessed 4th January 2018
-// note that query needs to be a single string with no line breaks so built
-//it up bit by bit
-var querystring = " SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM ";
-querystring = querystring + "(SELECT 'Feature' As type , ST_AsGeoJSON(lg.geom)::json As geometry, ";
-querystring = querystring + "row_to_json((SELECT l FROM (SELECT name, surname, port_id) As l ";
-querystring = querystring + " )) As properties";
-querystring = querystring + " FROM formdata As lg where lg.port_id = '"+req.params.port_id + "' limit 100 ) As f ";
+}
+var colnames = "";
+// first get a list of the columns that are in the table
+// use string_agg to generate a comma separated list that can then be
+//pasted into the next query
+var tablename = req.params.tablename;
+var geomcolumn = req.params.geomcolumn;
+var querystring = "select string_agg(colname,',') from ( select column_name as colname ";
+querystring = querystring + " FROM information_schema.columns as colname ";
+querystring = querystring + " where table_name =$1";
+querystring = querystring + " and column_name <> $2 and data_type <> 'USER-DEFINED') as cols ";
 console.log(querystring);
+// now run the query
+client.query(querystring,[tablename,geomcolumn], function(err,result){
+//call `done()` to release the client back to the pool
+done();
+if(err){
+console.log(err);
+res.status(400).send(err);
+}
+thecolnames = result.rows[0].string_agg;
+colnames = thecolnames;
+console.log("the colnames "+thecolnames);
+// now use the inbuilt geoJSON functionality
+// and create the required geoJSON format using a query adapted from
+//here:
+// http://www.postgresonline.com/journal/archives/267-Creating-
+//GeoJSON-Feature-Collections-with-JSON-and-PostGIS-functions.html, accessed 4th
+//January 2018
+// note that query needs to be a single string with no line breaks so
+//built it up bit by bit
+var querystring = " SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM ";
+querystring = querystring + "(SELECT 'Feature' As type , ST_AsGeoJSON(lg." + req.params.geomcolumn+")::json As geometry, ";
+querystring = querystring + "row_to_json((SELECT l FROM (SELECT "+colnames + ") As l )) As properties";
+// depending on whether we have a port number, do differen things
+if (req.params.portNumber) {
+querystring = querystring + " FROM "+req.params.tablename+" As lg where lg.port_id = '"+req.params.portNumber + "' limit 100 ) As f ";
+}
+else {
+querystring = querystring + " FROM "+req.params.tablename+" As lg limit 100 ) As f ";
+}
+console.log(querystring);
+// run the second query
 client.query(querystring,function(err,result){
 //call `done()` to release the client back to the pool
 done();
@@ -139,24 +171,11 @@ res.status(200).send(result.rows);
 });
 });
 });
-
-
-
-
-
-
-app.get('/',function(req,res){
-	res.send("hello world from the HTTP server");
 });
 
 
-//log the requests for debugging
-app.use(function(req,res,next){
-	var filename = path.basename(req.url);
-	var extension = path.extname(filename);
-	console.log("The file "+ filename + " was requested.");
-	next();
-});
+
+
 //serve static files e.g. html, css 
 //this should always be the last line in the server file
 app.use(express.static(__dirname));
